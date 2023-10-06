aa1c3c42af211c2b1d42cd5951c1a626667dfd36
Sabrina Hill <125325753+brinalynn17@users.noreply.github.com>
2023-10-06 22:19:46 +0000

    Excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+function randomElement(array) {
+  return array[Math.round(Math.random() * array.length)];
+
+}
+
+let excuse = randomElement(who) + " "+ randomElement(action)+" "+ randomElement(what)+" "+ randomElement(when);
+
+console.log(excuse)
+console.log("Hello Rigo from the console!");
 };
