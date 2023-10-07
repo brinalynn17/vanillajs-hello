53b46ceea1b2d541a233763a376b996f4f04a967
Sabrina Hill <125325753+brinalynn17@users.noreply.github.com>
2023-10-07 00:35:53 +0000

    I'm done

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/rigo-baby.jpg";
+import "./assets/img/Prince.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function excuse() {
